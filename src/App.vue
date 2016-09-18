7af71f3302fa63a7e03bffbf8694fad1a9431c4a
uknmr <y.kinmori@gmail.com>
2016-09-18 13:55:17 +0900

    ➕ srcを追加

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
+
+<script>
+import Hello from './components/Hello.vue';
+
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  },
+
+  components: {
+    Hello
+  },
+
+  created() {
+    console.log('hoge');
+  }
+}
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="hello">
+    <h1>{{ message }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
